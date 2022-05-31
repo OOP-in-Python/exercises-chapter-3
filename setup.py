ccd428271213916d2ef4a9118732a729bcc00d98
Indy Ng <writingindy@gmail.com>
2022-05-31 16:49:39 -0400

    Fix package name

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name="polynomials",
+    name="polynomial",
     version="0.1",
     packages=find_packages(),
-    install_requires=["trollius"]
 )
